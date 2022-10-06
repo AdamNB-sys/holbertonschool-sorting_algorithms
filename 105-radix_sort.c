68cb70e4d6d51caecee67e5152ced911eb7a130c
AdamNB-sys <a1adnat@gmail.com>
2022-10-06 13:54:03 -0500

    We're sortin baby!!

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -27,16 +27,47 @@ int maxInt(int *array, int size)
  */
 void radix_sort(int *array, size_t size)
 {
-    int digits = 0, x, y, max, num, power;
+    size_t digits = 0, x, y, max, num, power;
     int count[10];
     int *sorted_array;
 
-    if (array == NULL || size < 2)
+    if (!array || size < 2)
         return;
     max = maxInt(array, size);
     while (max > 0)
     {
         digits++;
         max /= 10;
+    }
+    power = 1;
+    for (x = 0; x < digits; x++)
+    {
+        sorted_array = malloc(sizeof(int) * size);
+        for (y = 0; y < 10; y++)
+        {
+            count[y] = 0;
+        }
+        for (y = 0; y < size; y++)
+        {
+            num = (array[y] / power) % 10;
+            count[num]++;
+        }
+        for (y = 1; y < 10; y++)
+        {
+            count[y] += count[y - 1];
+        }
+        for (y = size - 1; (int)y >= 0; y--)
+        {
+            num = (array[y] / power) % 10;
+            sorted_array[count[num] - 1] = array[y];
+            count[num]--;
+        }
+        for (y = 0; y < size; y++)
+        {
+            array[y] = sorted_array[y];
+        }
+        power *= 10;
+        free(sorted_array);
+        print_array(array, size);
     }
 }
